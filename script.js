b77e3384acc775753b91c8055e15f873f8e9d8ea
Mingmar Tamang <xm.tamang@yahoo.com>
2025-07-20 22:31:23 +0545

    add various html, css, js files

---
+++ script.js
@@ -0,0 +1,4 @@
+var typed = new Typed('#element', {
+	strings: ['Web Developer', 'Digital Marketing', 'Web Designer'],
+	typeSpeed: 60
+});
---
+++ script_about.js
@@ -0,0 +1,5 @@
+
+var typed = new Typed('#element_about', {
+	strings: ['Versatile professional with expertise in web design, web development, and digital marketing.', 'Skilled in crafting visually engaging and functional websites.', 'Experienced in developing robust and user-friendly web solutions.', 'Proficient in implementing strategic digital marketing campaigns.', 'Capable of creating cohesive online presences that captivate and drive results.'],
+	typeSpeed: 40
+})
