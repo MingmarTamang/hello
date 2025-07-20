ecd9ae1152d64a74ca4e2d1a4fbc7395828ec912
Mingmar Tamang <xm.tamang@yahoo.com>
2025-07-20 22:34:04 +0545

    delete some files

--- script_services.js
+++
@@ -1,5 +0,0 @@
-
-var typed = new Typed('#element_services', {
-	strings: ['1. Custom Website Desing', '2. Responsive Design', '3. Website Development', '4. Web Application Development', '5. Social Media Marketing', '6. Search Engine Optimization'],
-	typeSpeed: 40
-})
